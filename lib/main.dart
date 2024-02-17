54f93d0912f9ef8b1f38ff6053f2693a9d04eb96
feedapp <kaficsebaiust02@gmail.com>
2024-02-17 15:08:58 +0600

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:hackathon_sust/ui/screen/home_screen.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(
+
+      ),
+    );
+  }
+}
